5c9a8d381a4efc82a4369efe0e2372927ee293cd
anas110440 <75642207+anas110440@users.noreply.github.com>
2023-01-21 01:45:21 +0300

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
+import React from 'react';
+import {treeData} from "./data/data"
+import { Button, Checkbox, Form, Input, TreeSelect } from 'antd';
+
+
+const { SHOW_PARENT } = TreeSelect;
 
 function App() {
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    
+     
+      <Form
+    name="basic"
+   
+    style={{ width: "100%" }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+      <h3>
+        Please enter your name and pick the Sectors you are currently involved in.
+      </h3>
+    <Form.Item
+    name="name"
+      rules={[{ required: true, message: 'Please input your name!' }]}
+    >
+      <Input placeholder='Please input your name'  />
+    </Form.Item>
+    <Form.Item 
+            name="Sectors"
+
+       rules={[{ required: true, message: 'Need to be checked' }]}>
+      <TreeSelect
+        placeholder="Please select"
+        treeData={treeData}
+        allowClear
+        treeCheckable={true}
+        showCheckedStrategy={SHOW_PARENT}
+      />
+    </Form.Item>
+
+    <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+          },
+        ]}
+        
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+        </Form.Item>
+
+    <Form.Item >
+      <Button type="primary" htmlType="submit" style={{width:"100%"}}>
+        Save
+      </Button>
+    </Form.Item>
+  </Form>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,439 @@
+export const treeData = [
+    {
+      title: 'Manufacturing',
+      value: '0-0',
+      key: '0-0',
+      children: [
+        {
+          title: 'Construction materials',
+          value: '0-0-0',
+          key: '0-0-0',
+        },
+        {
+          title: 'Electronics and Optics',
+          value: '0-0-1',
+          key: '0-0-1',
+        },
+        {
+          title: 'Food and Beverage',
+          value: '0-0-2',
+          key: '0-0-2',
+          children: [
+            {
+              title: 'Bakery  confectionery products',
+              value: '0-0-2-0',
+              key: '0-0-2-0',
+            },
+            {
+              title: 'Beverages',
+              value: '0-0-2-1',
+              key: '0-0-2-1',
+            },
+            {
+              title: 'Fish fish products',
+              value: '0-0-2-2',
+              key: '0-0-2-2',
+            },
+            {
+              title: 'Meat meat products',
+              value: '0-0-2-3',
+              key: '0-0-2-3',
+            },
+            {
+              title: 'Milk dairy products',
+              value: '0-0-2-4',
+              key: '0-0-2-4',
+            },
+            {
+              title: 'Other',
+              value: '0-0-2-5',
+              key: '0-0-2-5',
+            },
+            {
+              title: 'Sweets snack food',
+              value: '0-0-2-6',
+              key: '0-0-2-6',
+            },
+          ],
+        },
+        {
+          title: 'Furniture',
+          value: '0-0-3',
+          key: '0-0-3',
+          children: [
+            {
+              title: 'Bathroom/sauna',
+              value: '0-0-3-0',
+              key: '0-0-3-0',
+            },
+            {
+              title: 'Bedroom',
+              value: '0-0-3-1',
+              key: '0-0-3-1',
+            },
+            {
+              title: 'Children’s room',
+              value: '0-0-3-2',
+              key: '0-0-3-2',
+            },
+            {
+              title: 'Kitchen',
+              value: '0-0-3-3',
+              key: '0-0-3-3',
+            },
+            {
+              title: 'Living room',
+              value: '0-0-3-4',
+              key: '0-0-3-4',
+            },
+            {
+              title: 'Office',
+              value: '0-0-3-5',
+              key: '0-0-3-5',
+            },
+            {
+              title: 'Other (Furniture)',
+              value: '0-0-3-6',
+              key: '0-0-3-6',
+            },
+            {
+              title: 'Outdoor',
+              value: '0-0-3-7',
+              key: '0-0-3-7',
+            },
+            {
+              title: 'Project furniture',
+              value: '0-0-3-8',
+              key: '0-0-3-8',
+            },
+          ],
+        },
+        {
+          title: 'Machinery',
+          value: '0-0-4',
+          key: '0-0-4',
+          children: [
+            {
+              title: 'Machinery components',
+              value: '0-0-4-0',
+              key: '0-0-4-0',
+            },
+            {
+              title: 'Machinery equipment/tools',
+              value: '0-0-4-1',
+              key: '0-0-4-1',
+            },
+            {
+              title: 'Manufacture of machinery',
+              value: '0-0-4-2',
+              key: '0-0-4-2',
+            },
+            {
+              title: 'Maritime',
+              value: '0-0-4-3',
+              key: '0-0-4-3',
+              children: [
+                {
+                  title: 'Aluminium and steel workboats',
+                  value: '0-0-4-3-0',
+                  key: '0-0-4-3-0',
+                },
+                {
+                  title: 'Boat/Yacht building',
+                  value: '0-0-4-3-1',
+                  key: '0-0-4-3-1',
+                },
+                {
+                  title: 'Ship repair and conversion',
+                  value: '0-0-4-3-2',
+                  key: '0-0-4-3-2',
+                },
+            
+              ],
+            },
+            {
+              title: 'Metal structures',
+              value: '0-0-4-4',
+              key: '0-0-4-4',
+            },
+            {
+              title: 'Other',
+              value: '0-0-4-5',
+              key: '0-0-4-5',
+            },
+            {
+              title: 'Repair and maintenance service',
+              value: '0-0-4-6',
+              key: '0-0-4-6',
+            }
+          ],
+        },
+        {
+          title: 'Metalworking',
+          value: '0-0-5',
+          key: '0-0-5',
+          children: [
+            {
+              title: 'Construction of metal structures',
+              value: '0-0-5-0',
+              key: '0-0-5-0',
+            },
+            {
+              title: 'Houses and buildings',
+              value: '0-0-5-1',
+              key: '0-0-5-1',
+            },
+            {
+              title: 'Metal products',
+              value: '0-0-5-2',
+              key: '0-0-5-2',
+            },
+            {
+              title: 'Metal works',
+              value: '0-0-5-3',
+              key: '0-0-5-3',
+              children: [
+                {
+                  title: 'CNC-machining',
+                  value: '0-0-5-3-0',
+                  key: '0-0-5-3-0',
+                },
+                {
+                  title: 'Forgings, Fasteners',
+                  value: '0-0-5-3-1',
+                  key: '0-0-5-3-1',
+                },
+                {
+                  title: 'Gas, Plasma, Laser cutting',
+                  value: '0-0-5-3-2',
+                  key: '0-0-5-3-2',
+                },
+                {
+                  title: 'MIG, TIG, Aluminum welding',
+                  value: '0-0-5-3-3',
+                  key: '0-0-5-3-3',
+                },
+            
+              ],
+            },
+          
+          ],
+        },
+        {
+          title: 'Plastic and Rubber',
+          value: '0-0-6',
+          key: '0-0-6',
+          children: [
+            {
+              title: 'Packaging',
+              value: '0-0-6-0',
+              key: '0-0-6-0',
+            },
+            {
+              title: 'Plastic goods',
+              value: '0-0-6-1',
+              key: '0-0-6-1',
+            },
+            
+            {
+              title: 'Plastic processing technology',
+              value: '0-0-6-3',
+              key: '0-0-6-3',
+              children: [
+                {
+                  title: 'Blowing',
+                  value: '0-0-6-2-0',
+                  key: '0-0-6-2-0',
+                },
+                {
+                  title: 'Moulding',
+                  value: '0-0-6-2-1',
+                  key: '0-0-6-2-1',
+                },
+                {
+                  title: 'Plastics welding and processing',
+                  value: '0-0-6-2-2',
+                  key: '0-0-6-2-2',
+                }
+            
+              ],
+            },
+            {
+              title: 'Plastic profiles',
+              value: '0-0-6-4',
+              key: '0-0-6-4',
+            },
+          
+          ],
+        },
+        {
+          title: 'Printing',
+          value: '0-0-7',
+          key: '0-0-7',
+          children: [
+            {
+              title: 'Advertising',
+              value: '0-0-7-0',
+              key: '0-0-7-0',
+            },
+            {
+              title: 'Book/Periodicals printing',
+              value: '0-0-7-1',
+              key: '0-0-7-1',
+            },
+            {
+              title: 'Labelling and packaging printing',
+              value: '0-0-7-2',
+              key: '0-0-7-2',
+            },
+          
+          ],
+        },
+        {
+          title: 'Textile and Clothing',
+          value: '0-0-8',
+          key: '0-0-8',
+          children: [
+            {
+              title: 'Clothing',
+              value: '0-0-8-0',
+              key: '0-0-8-0',
+            },
+            {
+              title: 'Textile',
+              value: '0-0-8-1',
+              key: '0-0-8-1',
+            }
+          
+          ],
+        },
+        {
+          title: 'Wood',
+          value: '0-0-9',
+          key: '0-0-9',
+          children: [
+            {
+              title: 'Other (Wood)',
+              value: '0-0-9-0',
+              key: '0-0-9-0',
+            },
+            {
+              title: 'Wooden building materials',
+              value: '0-0-9-1',
+              key: '0-0-9-1',
+            },
+            {
+              title: 'Wooden houses',
+              value: '0-0-9-2',
+              key: '0-0-9-2',
+            },
+          
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Other',
+      value: '0-1',
+      key: '0-1',
+      children: [
+        {
+          title: 'Creative industries',
+          value: '0-1-0',
+          key: '0-1-0',
+        },
+        {
+          title: 'Energy technology',
+          value: '0-1-1',
+          key: '0-1-1',
+        },
+        {
+          title: 'Environment',
+          value: '0-1-2',
+          key: '0-1-2',
+        },
+      ],
+    },
+    {
+      title: 'Service',
+      value: '0-2',
+      key: '0-2',
+      children: [
+        {
+          title: 'Business services',
+          value: '0-2-0',
+          key: '0-2-0',
+        },
+        {
+          title: 'Engineering',
+          value: '0-2-1',
+          key: '0-2-1',
+        },
+        {
+          title: 'Information Technology and Telecommunications',
+          value: '0-2-2',
+          key: '0-2-2',
+          children: [
+            {
+              title: 'Data processing, Web portals, E-marketing',
+              value: '0-2-2-0',
+              key: '0-2-2-0',
+            },
+            {
+              title: 'Programming, Consultancy',
+              value: '0-2-2-1',
+              key: '0-2-2-1',
+            },
+            {
+              title: 'Software, Hardware',
+              value: '0-2-2-2',
+              key: '0-2-2-2',
+            },
+            {
+              title: 'Telecommunications',
+              value: '0-2-2-3',
+              key: '0-2-2-3',
+            },
+          ],
+        },
+        {
+          title: 'Tourism',
+          value: '0-2-3',
+          key: '0-2-3',
+        },
+        {
+          title: 'Translation services',
+          value: '0-2-4',
+          key: '0-2-4',
+        },
+        {
+          title: 'Transport and Logistics',
+          value: '0-2-5',
+          key: '0-2-5',
+          children: [
+            {
+              title: 'Air',
+              value: '0-2-5-0',
+              key: '0-2-5-0',
+            },
+            {
+              title: 'Rail',
+              value: '0-2-5-1',
+              key: '0-2-5-1',
+            },
+            {
+              title: 'Road',
+              value: '0-2-5-2',
+              key: '0-2-5-2',
+            },
+            {
+              title: 'Water',
+              value: '0-2-5-3',
+              key: '0-2-5-3',
+            },
+          ],
+        },
+      ],
+    },
+    
+  ];
