68522e9e33937064113ae2ed96c6e05f3caf712d
anas110440 <75642207+anas110440@users.noreply.github.com>
2023-01-21 01:56:55 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,82 +1,76 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
 import React from 'react';
-import {treeData} from "./data/data"
+import { treeData } from './data/data';
 import { Button, Checkbox, Form, Input, TreeSelect } from 'antd';
-
 
 const { SHOW_PARENT } = TreeSelect;
 
 function App() {
-
   const onFinish = (values) => {
     console.log('Success:', values);
   };
-  
+
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
-  
-  
-
   return (
     <div className="container">
-    
-     
       <Form
-    name="basic"
-   
-    style={{ width: "100%" }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-      <h3>
-        Please enter your name and pick the Sectors you are currently involved in.
-      </h3>
-    <Form.Item
-    name="name"
-      rules={[{ required: true, message: 'Please input your name!' }]}
-    >
-      <Input placeholder='Please input your name'  />
-    </Form.Item>
-    <Form.Item 
-            name="Sectors"
-
-       rules={[{ required: true, message: 'Need to be checked' }]}>
-      <TreeSelect
-        placeholder="Please select"
-        treeData={treeData}
-        allowClear
-        treeCheckable={true}
-        showCheckedStrategy={SHOW_PARENT}
-      />
-    </Form.Item>
-
-    <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-          },
-        ]}
-        
+        name="basic"
+        style={{ width: '100%' }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Checkbox>
-          I have read the <a href="">agreement</a>
-        </Checkbox>
+        <h3>
+          Please enter your name and pick the Sectors you are currently involved
+          in.
+        </h3>
+        <Form.Item
+          name="name"
+          rules={[{ required: true, message: 'Please input your name!' }]}
+        >
+          <Input placeholder="Please input your name" />
+        </Form.Item>
+        <Form.Item
+          name="Sectors"
+          rules={[{ required: true, message: 'Need to be checked' }]}
+        >
+          <TreeSelect
+            placeholder="Please select"
+            treeData={treeData}
+            allowClear
+            treeCheckable={true}
+            showCheckedStrategy={SHOW_PARENT}
+          />
         </Form.Item>
 
-    <Form.Item >
-      <Button type="primary" htmlType="submit" style={{width:"100%"}}>
-        Save
-      </Button>
-    </Form.Item>
-  </Form>
+        <Form.Item
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject(new Error('Should accept agreement')),
+            },
+          ]}
+        >
+          <Checkbox>
+            I have read the <a href="">agreement</a>
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            Save
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
